30cba6ebe489bd47061b37139627952f18286464
Branko Blagojevic <branko.blagojevic@gmail.com>
2020-09-09 08:17:22 -0400

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import Grid from '../src/components/grid'
+
+export default function Home() {
+  return (
+    <Grid/>
+  )
+}
